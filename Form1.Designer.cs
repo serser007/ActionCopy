95fbfa2b47520ea54f1ade54642064302627b9c0
Sergey Baishev <bss.ser007@gmail.com>
2021-06-02 16:53:55 +0500

    Init

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace ActionCopy
+{
+    partial class ActionCopyForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.startServerButton = new System.Windows.Forms.Button();
+            this.updateTimer = new System.Windows.Forms.Timer(this.components);
+            this.startClientButton = new System.Windows.Forms.Button();
+            this.hostAddressBox = new System.Windows.Forms.TextBox();
+            this.clientSetupPanel = new System.Windows.Forms.Panel();
+            this.serversListBox = new System.Windows.Forms.ListBox();
+            this.actionCopyNotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.notifyContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.stopButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshServersButton = new System.Windows.Forms.Button();
+            this.clientSetupPanel.SuspendLayout();
+            this.notifyContextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // startServerButton
+            // 
+            this.startServerButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.startServerButton.Location = new System.Drawing.Point(0, 0);
+            this.startServerButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.startServerButton.Name = "startServerButton";
+            this.startServerButton.Size = new System.Drawing.Size(384, 44);
+            this.startServerButton.TabIndex = 0;
+            this.startServerButton.Text = "run server";
+            this.startServerButton.UseVisualStyleBackColor = true;
+            this.startServerButton.Click += new System.EventHandler(this.startServerButton_Click);
+            // 
+            // updateTimer
+            // 
+            this.updateTimer.Interval = 10;
+            this.updateTimer.Tick += new System.EventHandler(this.updateTimer_Tick);
+            // 
+            // startClientButton
+            // 
+            this.startClientButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.startClientButton.Location = new System.Drawing.Point(240, 0);
+            this.startClientButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.startClientButton.Name = "startClientButton";
+            this.startClientButton.Size = new System.Drawing.Size(144, 60);
+            this.startClientButton.TabIndex = 1;
+            this.startClientButton.Text = "run client";
+            this.startClientButton.UseVisualStyleBackColor = true;
+            this.startClientButton.Click += new System.EventHandler(this.startClientButton_Click);
+            // 
+            // hostAddressBox
+            // 
+            this.hostAddressBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.hostAddressBox.Location = new System.Drawing.Point(14, 16);
+            this.hostAddressBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.hostAddressBox.Name = "hostAddressBox";
+            this.hostAddressBox.Size = new System.Drawing.Size(219, 27);
+            this.hostAddressBox.TabIndex = 2;
+            this.hostAddressBox.Text = "127.0.0.1";
+            // 
+            // clientSetupPanel
+            // 
+            this.clientSetupPanel.BackColor = System.Drawing.SystemColors.Control;
+            this.clientSetupPanel.Controls.Add(this.hostAddressBox);
+            this.clientSetupPanel.Controls.Add(this.startClientButton);
+            this.clientSetupPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.clientSetupPanel.Location = new System.Drawing.Point(0, 44);
+            this.clientSetupPanel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.clientSetupPanel.Name = "clientSetupPanel";
+            this.clientSetupPanel.Size = new System.Drawing.Size(384, 60);
+            this.clientSetupPanel.TabIndex = 3;
+            // 
+            // serversListBox
+            // 
+            this.serversListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.serversListBox.FormattingEnabled = true;
+            this.serversListBox.ItemHeight = 20;
+            this.serversListBox.Location = new System.Drawing.Point(0, 104);
+            this.serversListBox.Name = "serversListBox";
+            this.serversListBox.Size = new System.Drawing.Size(384, 87);
+            this.serversListBox.TabIndex = 4;
+            this.serversListBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.serversListBox_DoubleClick);
+            // 
+            // actionCopyNotifyIcon
+            // 
+            this.actionCopyNotifyIcon.ContextMenuStrip = this.notifyContextMenuStrip;
+            this.actionCopyNotifyIcon.Text = "notifyIcon";
+            // 
+            // notifyContextMenuStrip
+            // 
+            this.notifyContextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stopButton});
+            this.notifyContextMenuStrip.Name = "notifyContextMenuStrip";
+            this.notifyContextMenuStrip.Size = new System.Drawing.Size(99, 26);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(98, 22);
+            this.stopButton.Text = "Stop";
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // refreshServersButton
+            // 
+            this.refreshServersButton.Location = new System.Drawing.Point(297, 111);
+            this.refreshServersButton.Name = "refreshServersButton";
+            this.refreshServersButton.Size = new System.Drawing.Size(75, 31);
+            this.refreshServersButton.TabIndex = 5;
+            this.refreshServersButton.Text = "refresh";
+            this.refreshServersButton.UseVisualStyleBackColor = true;
+            this.refreshServersButton.Click += new System.EventHandler(this.refreshServersButton_Click);
+            // 
+            // ActionCopyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(384, 191);
+            this.Controls.Add(this.refreshServersButton);
+            this.Controls.Add(this.serversListBox);
+            this.Controls.Add(this.clientSetupPanel);
+            this.Controls.Add(this.startServerButton);
+            this.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximumSize = new System.Drawing.Size(400, 230);
+            this.Name = "ActionCopyForm";
+            this.ShowIcon = false;
+            this.Text = "ActionCopy";
+            this.Load += new System.EventHandler(this.ActionCopyForm_Load);
+            this.clientSetupPanel.ResumeLayout(false);
+            this.clientSetupPanel.PerformLayout();
+            this.notifyContextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startServerButton;
+        private System.Windows.Forms.Timer updateTimer;
+        private System.Windows.Forms.Button startClientButton;
+        private System.Windows.Forms.TextBox hostAddressBox;
+        private System.Windows.Forms.Panel clientSetupPanel;
+        private System.Windows.Forms.ListBox serversListBox;
+        private System.Windows.Forms.NotifyIcon actionCopyNotifyIcon;
+        private System.Windows.Forms.ContextMenuStrip notifyContextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem stopButton;
+        private System.Windows.Forms.Button refreshServersButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using WindowsInput;
+using WindowsInput.Native;
+using ActionCopy.Properties;
+using Lidgren.Network;
+using VoidNetworking;
+using VoidNetworking.ModulesFramework;
+
+namespace ActionCopy
+{
+    [SuppressMessage("ReSharper", "UnusedMember.Local")]
+    public partial class ActionCopyForm : Form
+    {
+        [Module]
+        private static class Module
+        {
+            [ModuleReceiver(0)]
+            private static void OnMouseUpdate((int x, int y) point, NetConnection c)
+            {
+                if (isServer)
+                    return;
+                var position = new Point(point.x, point.y);
+                instance.SetCursorPosition(position);
+            }
+
+            [ModuleReceiver(1)]
+            private static void OnMouseUpdate(MouseButtons buttons, NetConnection c)
+            {
+                instance.SetMouseState(buttons);
+            }
+
+            [ModuleReceiver(2)]
+            private static void OnMouseUpdate(int wheel, NetConnection c)
+            {
+                instance.SetMouseWheel(wheel);
+            }
+
+            [ModuleReceiver(3)]
+            private static void OnKeyDownUpdate(Keys keys, NetConnection c)
+            {
+                instance.SetKeyDown(keys);
+            }
+
+            [ModuleReceiver(4)]
+            private static void OnKeyUpUpdate(Keys keys, NetConnection c)
+            {
+                instance.SetKeyUp(keys);
+            }
+        }
+
+        #region Extern
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+
+        private const int MouseEventLeftDown = 0x02;
+        private const int MouseEventLeftUp = 0x04;
+        private const int MouseEventRightDown = 0x08;
+        private const int MouseEventRightUp = 0x10;
+        private const int MouseEventMiddleDown = 0x20;
+        private const int MouseEventMiddleUp = 0x40;
+        private const int MouseEventWheel = 0x0800;
+
+        public static void DoMouseEvent(int value, int data=0)
+        {
+            mouse_event(value, 0, 0, data, 0);
+        }
+        #endregion
+
+        private static ActionCopyForm instance;
+
+        private const int Port = 8087;
+
+        private VoidServer server;
+        private VoidClient client;
+        private VoidPeer Peer => isServer ? (VoidPeer)server : client;
+        private readonly List<NetConnection> connections = new List<NetConnection>();
+        private static bool isServer;
+
+        private MouseButtons previousMouseButtonsState;
+        private UserActivityHook hook;
+        private InputSimulator simulator;
+
+        public ActionCopyForm()
+        {
+            instance = this;
+            InitializeComponent();
+        }
+
+        private void ActionCopyForm_Load(object sender, EventArgs e)
+        {
+            actionCopyNotifyIcon.Icon = Resources.icon;
+
+            client = new VoidClient();
+            client.OnServerDiscovery += point => serversListBox.Items.Add(point.Address);
+            client.SendDiscoveryRequest(Port);
+            
+            server = new VoidServer(Port);
+            server.OnConnected += connection => connections.Add(connection);
+            server.OnDisconnected += connection => connections.Remove(connection);
+
+            simulator = new InputSimulator();
+            hook = new UserActivityHook();
+            hook.OnMouseActivity += OnMouseActivity;
+            hook.KeyDown += (o, args) =>
+            {
+                if (isServer)
+                    connections.ForEach(c => server.SendMessage(3, args.KeyData, c));
+            };
+            hook.KeyUp += (o, args) =>
+            {
+                if (isServer)
+                    connections.ForEach(c => server.SendMessage(4, args.KeyData, c));
+            };
+        }
+
+        private void startServerButton_Click(object sender, EventArgs e)
+        {
+            connections.Clear();
+
+            isServer = true;
+            server.Start();
+            updateTimer.Enabled = true;
+            SetUiActiveState();
+        }
+        
+        private void startClientButton_Click(object sender, EventArgs e)
+        {
+            client = new VoidClient();
+            client.Connect(hostAddressBox.Text, Port);
+            updateTimer.Enabled = true;
+            SetUiActiveState();
+        }
+
+        private void updateTimer_Tick(object sender, EventArgs e)
+        {
+            Peer.RunQueuedActions();
+            if (isServer)
+            {
+                var point = (Cursor.Position.X, Cursor.Position.Y);
+                connections.ForEach(c => server.SendMessage(0, point, c));
+                connections.ForEach(c => server.SendMessage(1, MouseButtons, c));
+            }
+            else
+            {
+                if (!client.IsAlive)
+                {
+                    Reset();
+                    MessageBox.Show("Unable to connect");
+                }
+            }
+        }
+
+        
+        private void OnMouseActivity(object sender, MouseEventArgs e)
+        {
+            if (!isServer)
+                return;
+            if (e.Button != MouseButtons.None)
+            {
+                connections.ForEach(c => server.SendMessage(1, e.Button, c));
+            }
+
+            if (e.Delta != 0)
+            {
+                connections.ForEach(c => server.SendMessage(2, e.Delta, c));
+            }
+        }
+
+        private void SetCursorPosition(Point position)
+        {
+            Cursor.Position = position;
+        }
+
+        private void SetMouseState(MouseButtons buttons)
+        {
+            if (IsLeftPressed(buttons) && !IsLeftPressed(previousMouseButtonsState))
+                DoMouseEvent(MouseEventLeftDown);
+            if (!IsLeftPressed(buttons) && IsLeftPressed(previousMouseButtonsState))
+                DoMouseEvent(MouseEventLeftUp);
+
+            if (IsRightPressed(buttons) && !IsRightPressed(previousMouseButtonsState))
+                DoMouseEvent(MouseEventRightDown);
+            if (!IsRightPressed(buttons) && IsRightPressed(previousMouseButtonsState))
+                DoMouseEvent(MouseEventRightUp);
+
+            if (IsMiddlePressed(buttons) && !IsMiddlePressed(previousMouseButtonsState))
+                DoMouseEvent(MouseEventMiddleDown);
+            if (!IsMiddlePressed(buttons) && IsMiddlePressed(previousMouseButtonsState))
+                DoMouseEvent(MouseEventMiddleUp);
+
+            previousMouseButtonsState = buttons;
+        }
+
+        private void SetMouseWheel(int wheel)
+        {
+            DoMouseEvent(MouseEventWheel, wheel);
+        }
+
+        private void SetKeyDown(Keys keys)
+        {
+            simulator.Keyboard.KeyDown((VirtualKeyCode) ((int) keys));
+        }
+
+        private void SetKeyUp(Keys keys)
+        {
+            simulator.Keyboard.KeyUp((VirtualKeyCode) ((int) keys));
+        }
+
+        private bool IsLeftPressed(MouseButtons buttons) => (buttons & MouseButtons.Left) > 0;
+        private bool IsRightPressed(MouseButtons buttons) => (buttons & MouseButtons.Right) > 0;
+        private bool IsMiddlePressed(MouseButtons buttons) => (buttons & MouseButtons.Middle) > 0;
+
+        private void serversListBox_DoubleClick(object sender, MouseEventArgs e)
+        {
+            var index = serversListBox.IndexFromPoint(e.Location);
+            if (index == ListBox.NoMatches)
+                return;
+            hostAddressBox.Text = serversListBox.Items[index].ToString();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void Reset()
+        {
+            Peer.Shutdown();
+            isServer = false;
+            SetUiActiveState(true);
+            connections.Clear();
+            updateTimer.Enabled = false;
+        }
+
+        private void SetUiActiveState(bool isActive=false)
+        {
+            Visible = isActive;
+            actionCopyNotifyIcon.Visible = !isActive;
+        }
+
+        private void refreshServersButton_Click(object sender, EventArgs e)
+        {
+            client = new VoidClient();
+            client.OnServerDiscovery += point => serversListBox.Items.Add(point.Address);
+            serversListBox.Items.Clear();
+            client.SendDiscoveryRequest(Port);
+        }
+    }
+}
